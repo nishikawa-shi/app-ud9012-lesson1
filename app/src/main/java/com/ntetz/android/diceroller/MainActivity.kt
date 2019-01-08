0cfa7b8d30cc0c17812f9187bd90961ddf068e26
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2019-01-08 21:52:39 +0900

    サイコロを振るボタンを押した時のポップアップが出ないようにした

--- app/src/main/java/com/ntetz/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/ntetz/android/diceroller/MainActivity.kt
@@ -3,7 +3,6 @@ package com.ntetz.android.diceroller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,7 +12,6 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = this.findViewById(R.id.roll_button)
         rollButton.text = this.getString(R.string.roll_button_default)
         rollButton.setOnClickListener {
-            Toast.makeText(this, R.string.roll_message, Toast.LENGTH_SHORT).show()
         }
     }
 }
