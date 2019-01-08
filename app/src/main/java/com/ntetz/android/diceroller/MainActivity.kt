aa0627ac0b17bbdcac0b23cb83229fd02f6bf44b
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2019-01-08 22:59:50 +0900

    サイコロを振るボタンを押した時、ランダムでサイコロの画像が表示されるようにした
    不要なimportを削除した

--- app/src/main/java/com/ntetz/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/ntetz/android/diceroller/MainActivity.kt
@@ -3,7 +3,7 @@ package com.ntetz.android.diceroller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -22,5 +22,16 @@ class MainActivity : AppCompatActivity() {
         val offset = 1
         val range = 6
         val randomInt = Random().nextInt(range) + offset
+        val diceImage: ImageView = this.findViewById(R.id.dice_image)
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> return
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }
