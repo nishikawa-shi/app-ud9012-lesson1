cc33899da6916510b67b805792178eb7d9bc4560
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2019-01-08 18:00:34 +0900

    サイコロを振るボタンを押した時、ポップアップが出るようにした

--- app/src/main/java/com/ntetz/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/ntetz/android/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ntetz.android.diceroller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +12,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton: Button = this.findViewById(R.id.roll_button)
         rollButton.text = this.getString(R.string.roll_button_default)
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
