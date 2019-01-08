7e8603effa47bbecabc73fdd234ecfe4fe91365b
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2019-01-08 21:58:30 +0900

    サイコロを振るボタンを押した時、結果として賽の目の数字が更新されるようにした

--- app/src/main/java/com/ntetz/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/ntetz/android/diceroller/MainActivity.kt
@@ -3,6 +3,8 @@ package com.ntetz.android.diceroller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +14,15 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = this.findViewById(R.id.roll_button)
         rollButton.text = this.getString(R.string.roll_button_default)
         rollButton.setOnClickListener {
+            this.rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val offset = 1
+        val range = 6
+        val randomInt = Random().nextInt(range) + offset
+        val resultText: TextView = this.findViewById(R.id.result_text)
+        resultText.text = randomInt.toString()
     }
 }
