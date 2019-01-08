5eac7e6cccce6f10746c7b822cf4f956a4718a32
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2019-01-08 17:54:06 +0900

    サイコロを振るボタンのラベルを動的に設定できるようにした

--- app/src/main/java/com/ntetz/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/ntetz/android/diceroller/MainActivity.kt
@@ -2,11 +2,14 @@ package com.ntetz.android.diceroller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = this.findViewById(R.id.roll_button)
+        rollButton.text = this.getString(R.string.roll_button_default)
     }
 }
